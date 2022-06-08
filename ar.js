9f1c4bf1aaddeb6234a72a5d87b8986a6f71ad32
sharad-bapat <87427801+sharad-bapat@users.noreply.github.com>
2022-06-08 10:34:19 +0530

    Add files via upload

--- ar.js
+++ ar.js
@@ -4,12 +4,9 @@ function getArticleExtract(url) {
         (next) => {
             Parse(`${url}`).then(data => {
                 if (data.title) {
-                    // console.clear()
-                    $("#wpContent").append(`<h1>${data.title}</h1>`);
-                    $("#wpContent").append(`<h2>${data.date_published}</h2>`);
-                    $("#wpContent").append(`<img src ="${data.lead_image_url} alt="" width='100%' height="auto" style="object-fit:cover" onerror='imgError(this)'/>`);
-                    $("#wpContent").append(`<p class="small">${data.content}<p>`);
-                    $("#wpContent").append(`<p class="small d-flex-justify-content-center">Via: Normal Parse/<p>`);
+                    console.log(data);
+                    source_url = new URL(url);
+                    update_HTML(data, source_url);
                 } else {
                     console.log(`Simple parsing did not work`);
                     return next(new Error('Cannot get Data'))
@@ -18,76 +15,16 @@ function getArticleExtract(url) {
                 console.log(err);
                 return next(new Error('Cannot get Data'))
             })
-        },
-        // (next) => {
-        //     fetchURL(`https://api.outline.com/v3/parse_article?source_url=${url}`).then(data => {
-        //         if (data.success) {
-        //             data = data.data;
-        //             if (data.data.site_name == "Outline") {
-        //                 return next(new Error('Cannot get Data'))
-        //             } else {
-        //                 // console.clear()
-        //                 let title = data.data.title ? data.data.title : ``;
-        //                 let content = data.data.html ? data.data.html : ``;
-        //                 let pageUrl = data.data.article_url ? data.data.article_url : ``;
-        //                 let icon = data.data.icon ? data.data.icon : ``;
-        //                 let author = data.data.author ? data.data.author : ``;
-        //                 let siteName = data.data.site_name ? data.data.site_name : ``;
-        //                 let date = data.data.date ? data.data.date : ``;
-        //                 $("#wpContent").append(`<h1>${title}</h1>`);
-        //                 $("#wpContent").append(`<h2>${date}</h2>`);
-        //                 $("#wpContent").append(`<p class="small">${content}<p>`);
-        //                 $("#wpContent").append(`<p class="small d-flex-justify-content-center">Via: Outline.com/<p>`);
-        //             }
-
-        //         } else {
-        //             console.log(`Outline did not work`);
-        //             return next(new Error('Cannot get Data'))
-        //         }
-        //     }).catch(err => {
-        //         console.log(err);
-        //         return next(new Error('Cannot get Data'))
-        //     })
-        // },
-        (next) => {
-            fetchURL(`https://api-panda.com/v2/feeds/story/full?url=${url}`).then(data => {
-                if (data.success) {
-                    // console.clear()
-                    data = data.data;
-                    console.log(data);
-                    // console.log(data.data.data.title);
-                    let title = data.data.title ? data.data.title : ``;
-                    let content = data.data.html ? data.data.html : ``;
-                    let pageUrl = data.data.pageUrl ? data.data.pageUrl : ``;
-                    let icon = data.data.icon ? data.data.icon : ``;
-                    let author = data.data.author ? data.data.author : ``;
-                    let authorUrl = data.data.authorUrl ? data.data.authorUrl : ``;
-                    let siteName = data.data.siteName ? data.data.siteName : ``;
-                    let date = data.data.date ? new Date(data.data.date).toLocaleString("en-GB") : ``;
-                    console.log(title,content,date);
-                    $("#wpContent").append(`<h1>${title}</h1>`);
-                    $("#wpContent").append(`<h2>${date}</h2>`);
-                    $("#wpContent").append(`<p class="small">${content}<p>`);
-                    $("#wpContent").append(`<p class="small d-flex-justify-content-center">Via: UsePanda.com/<p>`);
-                } else {
-                    console.log(`Pandas did not work`);
-                    return next(new Error('Cannot get Data'))
-                }
-            }).catch(err => {
-                console.log(err);
-                return next(new Error('Cannot get Data'))
-            })
-        },
+        },        
 
         (next) => {
+
             Parse(`https://sbcors.herokuapp.com/${url}`).then(data => {
                 if (data.title) {
-                    // console.clear()
-                    $("#wpContent").append(`<h1>${data.title}</h1>`);
-                    $("#wpContent").append(`<h2>${data.date_published}</h2>`);
-                    $("#wpContent").append(`<img src ="${data.lead_image_url} alt="" width='100%' height="auto" style="object-fit:cover" onerror='imgError(this)'/>`);
-                    $("#wpContent").append(`<p class="small">${data.content}<p>`);
-                    $("#wpContent").append(`<p class="small d-flex-justify-content-center">Via: Proxy server/<p>`);
+                    console.log(data);
+                    source_url = new URL(url);
+                    update_HTML(data, source_url);
+                   
                 } else {
                     console.log(`CORS did not work`);
                     return next(new Error('Cannot get Data'))
@@ -96,24 +33,23 @@ function getArticleExtract(url) {
                 console.log(err);
                 return next(new Error('Cannot get Data'))
             })
-        },
-        (next) => {
-            fetchURL(`https://txtify.it/${url}`).then(data => {
-                if (data.success) {
-
-                } else {
-                    // console.clear()
-                    $("#wpContent").append(data.response);
-                    $("#wpContent").append(`<p class="small d-flex-justify-content-center">Via: txtify.it/<p>`);
-                }
-            }).catch(err => {
-                console.log(err);
-                return next(new Error('Cannot get Data'))
-            })
-        },
+        },        
     ])
-    $("#loader").attr("style", "none");
+    $("#loader").attr("style", "display:none");
 
+}
+
+function update_HTML(data, source_url) {
+    $("#wpContent").append(`<img alt="${source_url.hostname}" class="mt-2 mb-3" src="https://www.google.com/s2/favicons?domain=${source_url.hostname}" /><span class="small"> ${source_url.hostname.replace("http://", "").replace("https://", "").replace("www.", "") }</span>`);
+    $("#wpContent").append(`<h1>${data.title}</h1>`);
+    if (data.date_published) {
+        $("#wpContent").append(`<p class="small m-2">${data.date_published.split('T')[0]}</p>`);
+    }    
+    $("#wpContent").append(`<hr class="my-3"></hr>`);
+    $("#wpContent").append(`<img src ="${data.lead_image_url} alt="" width='100%' height="auto" style="object-fit:cover" onerror='imgError(this)'/>`);
+    $("#wpContent").append(`<p class="small">${data.content}<p>`);    
+    $("#loader").attr("style", "display:none");
+    $("#wpContent").show();
 }
 
 async function fetchURL(url) {
@@ -135,9 +71,19 @@ function imgError(image) {
 
 $("#button-analyze").on("click", (function (e) { 
     e.preventDefault();
-    $("#loader").attr("style", "block");
-    $("#wpContent").hide();
+    $("#loader").attr("style", "display:block");
+    //$("#wpContent").hide();
     getArticleExtract($("#input-url").val().trim());
 }));
    
 
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+const url = urlParams.get('url');
+console.log(url);
+if (url) {
+    //$("#loader").attr("style", "block");
+    $("#wpContent").hide();
+    getArticleExtract(url.trim());
+}
